3d14f738b1ba0c7605bf3bf15e0bf282a926a2c0
sergiopb06 <sergiopb06@gmail.com>
2026-07-07 12:43:17 -0600

    Update compileWindows.bat

--- compileWindows.bat
+++ compileWindows.bat
@@ -1,4 +1,10 @@
 @echo off
+cd /d "%~dp0"
+
+set SFML_INC=C:\msys64\mingw64\include
+set SFML_LIB=C:\msys64\mingw64\lib
+set SFML_BIN=C:\msys64\mingw64\bin
+
 g++ -std=c++17 -Wall ^
     src\Code\Graph\Graph.cpp ^
     src\Code\MazeGenerator\MazeGenerator.cpp ^
@@ -11,6 +17,8 @@ g++ -std=c++17 -Wall ^
     src\GUI\SerialReader.cpp ^
     src\GUI\asm\int_to_string.o ^
     -I src ^
+    -I "%SFML_INC%" ^
+    -L "%SFML_LIB%" ^
     -o src\GUI\pacman ^
     -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 
@@ -18,6 +26,8 @@ if %errorlevel% neq 0 (
     pause
     exit /b %errorlevel%
 )
+
+copy /Y "%SFML_BIN%\*.dll" src\GUI\ >nul
 
 cd src\GUI
 pacman.exe
